d260c960d9d1ca36d754963be260613a025851b9
vamshinilagiri2 <nilagirivamshi2000@gmail.com>
2022-12-04 20:38:42 -0500

    added booking history page-modification

--- Project/js/orderhistory.js
+++ Project/js/orderhistory.js
@@ -38,46 +38,6 @@ async function orderhistory() {
     else if (response.ok == false) {
 
     }
-
-
-
-
-    const response2 = await fetch(api_url2);
-    const data2 = await response2.json();
-    console.log(data2)
-    if (response2.ok == true) {
-        for (let i of data2.moviesList) {
-            resultGrid2.innerHTML += `
-                <div class="swiper-slide">
-                    <div class="movie-box">
-                        <img src="data:image/png;base64,${i.picture}" alt="" class="movie-box-img">
-                        <div class="box-text">
-                            <h2 class="movie-title">${i.title}</h2>
-                            <span class="movie-type">${i.category}</span>
-                            <br>
-                            <a href="${i.trailerLink}" class="book-btn">
-                                <span>View Trailer</span> 
-                                <lord-icon src="https://cdn.lordicon.com/fetyzpiw.json" trigger="morph" colors="primary:#ffffff" style="width:25px;height:25px"> </lord-icon>
-                            </a>
-                            <a href="movieinformation.html?title=${i.title}" class="book-btn" onclick="moviecarddetails()">
-                            <span>Book ticket</span> 
-                            <lord-icon src="https://cdn.lordicon.com/ucvsemjq.json" trigger="hover" colors="primary:#ffffff" style="width:25px;height:25px"> </lord-icon>
-                            </a>
-                        </div>
-                    </div>  
-                </div>
-        
-        `;
-        }
-
-    }
-    else if (response2.ok == false) {
-
-    }
-
-
-
-
 }
 
 
