e769ea0ae9aa55cd2f5a3292c030a9a50eb8216e
vamshinilagiri2 <nilagirivamshi2000@gmail.com>
2022-12-08 00:06:14 -0500

    order history modifications

--- Project/js/orderhistory.js
+++ Project/js/orderhistory.js
@@ -1,35 +1,37 @@
 const resultGrid1 = document.getElementById('orderhistorycard');
-const api_url = "http://localhost:8080/user/movies/current";
+const api_url = "http://localhost:8080/checkout/orderHistory/" +un;
 
 async function orderhistory() {
     const response = await fetch(api_url);
     const data = await response.json();
     console.log(data)
     if (response.ok == true) {
-        for (let i of data.moviesList) {
+        for (let i of data.bookingList) {
             resultGrid1.innerHTML += `
-            <div class="product__photo">
-                <div class="photo-main">
-                    <img src="imgs/alien.jpg" alt="green apple slice">
+            <section class="product" id="orderhistorycards">
+                <div class="product__photo">
+                    <div class="photo-main">
+                    <img src="data:image/png;base64,${i.movie.picture}" alt="" class="movie-box-img">
+                    </div>
                 </div>
-            </div>
-            <div class="product__info">
-                <div class="title">
-                    <h1>Movie Name </h1>
-                    <span>Booking ID: 45999</span>
+                <div class="product__info">
+                    <div class="title">
+                        <h1>${i.movie.title}</h1>
+                        <span>Booking ID: CEBS${i.bookingID}</span>
+                    </div>
+                    <div class="description">
+                        <ul>
+                            <li>Show Date: ${i.showdetails.showId.showDate}</li>
+                            <li>Show time: ${i.showdetails.showId.showTime}</li>
+                            <li>Number of tickets purchased: x ${i.numberOfTickets}</li>
+                        </ul>
+                    </div>
+                    <div class="price">
+                        <span>$${i.totalPrice}</span>
+                    </div>
                 </div>
-                <div class="description">
-                    <ul>
-                        <li>Show Date: 01-01-01</li>
-                        <li>Show time: 10:00:00</li>
-                        <li>Number of tickets purchased: x4</li>
-                    </ul>
-                </div>
-                <div class="price">
-                    <span>$7.93</span>
-                </div>
-            </div>
-        
+          </section>
+          <br>
         `;
         }
 
